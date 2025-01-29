e57c5500379c7cd8f3561452c74912e9da6db247
cephus <pnakitaren2@gmail.com>
2025-01-29 22:54:56 +0300

    task:Home UI custom

--- src/app/components/Detail.jsx
+++ src/app/components/Detail.jsx
@@ -12,7 +12,7 @@ const Detail = () => {
           <div className='mt-10 space-y-7 mx-3 text-center pt-20'>
             <p className={styles.sectionSubText}>A Customised Relocation Experience</p>
             <h1 className={styles.heroHeadText}>Atlas Path – Specialists in Exceptional Moving Services</h1>
-            <p className={`${styles.paragraph}`}>Atlas Path Relocation is your trusted partner for a stress-free and seamless move across borders. As one of the leading international moving companies, we specialise in providing door-to-door moving services. Are you looking for local relocation service or a international moving company? Santa Fe is on hand to support you and your family.</p>
+            <p className={`${styles.paragraph}`}>Atlas Path Relocation is your reliable companion for a hassle-free and smooth transition across countries. As a top moving provider, we excel in offering comprehensive door-to-door relocation solutions. Whether you need a local, corporate moving service or an international relocation expert, Atlas Path is here to assist you and your loved ones.</p>
             
             <div className='flex items-center justify-center'>                  
             <Link href={"/contact"} className='w-full border w-1/2  flex justify-center border-grade mb-10 mt-10 text-center px-5 py-4 text-lg font-bold text-primary hover:text-white hover:bg-primary hover:scale-105 duration-500 border-l-[5px]'>Book a Consultation Today !!</Link>
--- src/app/components/Quiz.jsx
+++ src/app/components/Quiz.jsx
@@ -5,7 +5,7 @@ const Quiz = () => {
   return (
     <div className='pt-20 mt-20 mb-20 space-y-4 mx-3'>
         <h1 className='text-4xl tracking-wide font-bold'>Connect with Us?</h1>
-        <p className='text-gray-700 font-normal tracking-wide text-2xl'>Give our team of friendly international moving experts a call or drop us a note.</p>
+        <p className='text-gray-700 font-normal tracking-wide text-2xl'>Give our team of friendly relocation moving experts a call or drop us a note.</p>
         <div className='flex justify-center'>
         <GetStarted/>
         </div>
