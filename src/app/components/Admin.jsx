d792de02a37ee55cc239423eb4bd645ee0548c8d
cephus <pnakitaren2@gmail.com>
2025-01-30 20:34:53 +0300

    chore:admin

--- src/app/components/Admin.jsx
+++ src/app/components/Admin.jsx
@@ -12,10 +12,6 @@ const Admin = () => {
                 src={finesse}
                 alt='finesse'
                 className='w-7 h-7'/>
-                <div className='flex space-x-3'>
-                    <div className='w-6 h-6 mt-1 rounded-lg bg-black-gradient'/>
-                    <p className={styles.sectionSubText}>ADMIN SUPPORT</p>
-                </div>
                 <h1 className={styles.sectionHeadText}>Helping You Get Started</h1>
                 <h2 className='uppercase tracking-wide text-center font-bold text-dark text-lg mt-5 mb-3'>COMMITTED RELOCATION SPECIALIST</h2>
                 <p className={`${styles.paragraph} w-3/4 text-center`}>Atlas Path assigns a dedicated relocation specialist to support you throughout your move. They manage coordination, packing, shipment tracking, and international customs procedures. With their expertise, your relocation is streamlined and hassle-free.</p>
