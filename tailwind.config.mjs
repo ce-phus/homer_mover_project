76aca95b958aed766de30de6f6f3e975fe00aa81
cephus <pnakitaren2@gmail.com>
2025-02-10 22:01:07 +0300

    chore:Image custom

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -41,11 +41,11 @@ module.exports = {
        "2xl": { max: "1535px" },
        // => @media (max-width: 1535px) { ... }
    
-       xl: { max: "1279px" },
-       // => @media (max-width: 1279px) { ... }
+       xl: { max: "1280px" },
+       // => @media (max-width: 1280px) { ... }
    
-       lg: { max: "1023px" },
-       // => @media (max-width: 1023px) { ... }
+       lg: { max: "1024px" },
+       // => @media (max-width: 1024px) { ... }
    
        md: { max: "767px" },
        // => @media (max-width: 767px) { ... }
