ab2d36b09fb1e524b527b5eca385a7213bd1d825
cephus <pnakitaren2@gmail.com>
2025-02-12 21:47:16 +0300

    chore:Testimonials custom UI

--- src/app/components/Testimonials.jsx
+++ src/app/components/Testimonials.jsx
@@ -11,7 +11,7 @@ const Testimonials = () => {
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 3, // Show 2 cards at a time
+        slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 4000,
@@ -19,14 +19,14 @@ const Testimonials = () => {
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 2, // Show 2 cards on medium screens
+                    slidesToShow: 2, 
                     slidesToScroll: 1,
                 }
             },
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 1, // Show 1 card on smaller screens
+                    slidesToShow: 1, 
                     slidesToScroll: 1,
                 }
             }
